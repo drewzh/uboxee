4eca7f6e7f92c0e47235f4ce5a2aa686fafb714c
Andrew Higginson <azhigginson@gmail.com>
2010-12-21 15:06:36 +0000

    Added CPU architecture detection and support for 32bit/64bit package installation

--- uboxee_setup.sh
+++ uboxee_setup.sh
@@ -14,91 +14,116 @@ fi
 echo "Please note, this script is for 64bit users ONLY, press ENTER to continue or CTRL+C to cancel..."
 read
 
+#VARIABLES
+architecture=`uname -m`
+
 wizard(){
     echo "Customising Ubuntu please wait..."
 
     echo "Adding repository information..."
-    sudo add-apt-repository ppa:tualatrix/ppa
+    add-apt-repository ppa:tualatrix/ppa
     wget --output-document=/etc/apt/sources.list.d/medibuntu.list http://www.medibuntu.org/sources.list.d/$(lsb_release -cs).list
     apt-get --quiet update
     apt-get --yes --quiet --allow-unauthenticated install medibuntu-keyring
     apt-get --quiet update
     apt-get --yes install app-install-data-medibuntu apport-hooks-medibuntu
-    sudo add-apt-repository ppa:tualatrix/ppa
-    sudo add-apt-repository ppa:nvidia-vdpau/ppa
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CEC06767
-    sudo add-apt-repository ppa:team-xbmc-svn/ppa
-    sudo add-apt-repository ppa:jcfp/ppa
-    sudo add-apt-repository ppa:tualatrix/ppa
-    sudo add-apt-repository ppa:pmcenery/ppa
-    sudo add-apt-repository ppa:ubuntu-wine/ppa
-    sudo add-apt-repository ppa:deja-dup-team/ppa
-    sudo add-apt-repository ppa:elementaryart
-    sudo add-apt-repository ppa:am-monkeyd/nautilus-elementary-ppa
-    sudo add-apt-repository ppa:gloobus-dev/gloobus-preview
-    sudo add-apt-repository ppa:awn-testing/ppa
-    sudo apt-get update
-    sudo apt-get dist-upgrade
+    add-apt-repository ppa:tualatrix/ppa
+    add-apt-repository ppa:nvidia-vdpau/ppa
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CEC06767
+    add-apt-repository ppa:team-xbmc-svn/ppa
+    add-apt-repository ppa:jcfp/ppa
+    add-apt-repository ppa:tualatrix/ppa
+    add-apt-repository ppa:pmcenery/ppa
+    add-apt-repository ppa:ubuntu-wine/ppa
+    add-apt-repository ppa:deja-dup-team/ppa
+    add-apt-repository ppa:elementaryart
+    add-apt-repository ppa:am-monkeyd/nautilus-elementary-ppa
+    add-apt-repository ppa:gloobus-dev/gloobus-preview
+    add-apt-repository ppa:awn-testing/ppa
+    apt-get update
+    apt-get dist-upgrade
 
     echo "Setting up unattented updates..."
-    sudo apt-get install unattended-upgrades
-    sudo dpkg-reconfigure unattended-upgrades
+    apt-get install unattended-upgrades
+    dpkg-reconfigure unattended-upgrades
+    
+    echo "Installing SSH..."
+    apt-get install openssh-server
 
     echo "Installing Restricted Extras (installs java, flash and  a few other essential commercial products)"
-    sudo apt-get install ubuntu-restricted-extras
+    apt-get install ubuntu-restricted-extras
 
     echo "Installing Ubuntu Tweak..."
-    sudo apt-get install ubuntu-tweak
+    apt-get install ubuntu-tweak
 
     echo "Installing Medibuntu..."
     apt-get install libdvdcss2
-    apt-get install w64codecs
+    if [ "$architecture" != "x86_64" ] && [ "$architecture" != "ia64" ]; then
+        apt-get install w32codecs
+    else
+        apt-get install w64codecs
+    fi    
 
     echo "Installing Nvidia related drivers and extras..."
-    sudo apt-get install vdpauinfo libvdpau1 nvidia-current nvidia-190-modaliases nvidia-glx-190 nvidia-settings-190 hd-widescreen-wallpapers
+    apt-get install vdpauinfo libvdpau1 nvidia-current nvidia-190-modaliases nvidia-glx-190 nvidia-settings-190 hd-widescreen-wallpapers
 
     echo "Installing Boxee..."
-    wget http://dl.boxee.tv/boxee-0.9.22.13692.i486.modfied.deb --output-document=/tmp/boxee.deb
+    if [ "$architecture" != "x86_64" ] && [ "$architecture" != "ia64" ]; then
+        wget http://dl.boxee.tv/boxee-0.9.22.13692.i486.modfied.deb --output-document=/tmp/boxee.deb
+    else
+        wget http://dl.boxee.tv/boxee-0.9.22.13692.x86_64.modfied.deb --output-document=/tmp/boxee.deb
+    fi
+        
     dpkg -i /tmp/boxee.deb
     rm -f /tmp/boxee.deb
 
     echo "Installing XBMC Media Center..."
-    sudo apt-get install xbmc
+    apt-get install xbmc
 
     echo "Installing sabnzbdplus..."
-    sudo apt-get install sabnzbdplus
+    apt-get install sabnzbdplus
 
     echo "Installing Ubuntu Tweak..."
-    sudo apt-get install ubuntu-tweak
+    apt-get install ubuntu-tweak
 
     echo "Installing IPhone driver..."
-    sudo apt-get install gvfs ipheth-utils
+    apt-get install gvfs ipheth-utils
 
     echo "Installing Official Java Package..."
-    sudo apt-get install sun-java6-jdk
+    apt-get install sun-java6-jdk
 
     echo "Installing Wine..."
-    sudo apt-get install wine
+    apt-get install wine
 
     echo "Installing Deja-Dup Backup..."
-    sudo apt-get install deja-dup
+    apt-get install deja-dup
 
     echo "Installing Elementary Theme..."
-    sudo apt-get install elementaryart
+    apt-get install elementaryart
     nautilus -q
 
     echo "Installing Gloobus..."
-    sudo apt-get install gloobus-preview
+    apt-get install gloobus-preview
 
     echo "Installing AWN..."
-    sudo apt-get install awn
+    apt-get install awn
 
     echo "Installing Google Chrome..."
-    sudo apt-get remove firefox
-    wget http://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb --output-document=/tmp/chrome.deb
+    apt-get remove firefox
+    
+    echo "Installing Boxee..."
+    if [ "$architecture" != "x86_64" ] && [ "$architecture" != "ia64" ]; then
+        wget http://dl.google.com/linux/direct/google-chrome-unstable_current_i386.deb --output-document=/tmp/chrome.deb
+    else
+        wget http://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb --output-document=/tmp/chrome.deb
+    fi
+    
     dpkg -i /tmp/chrome.deb
     rm -f /tmp/chrome.deb
 
+    #echo "Installing boxee splash screen..."
+    #update-alternatives --install /usr/lib/usplash/usplash-artwork.so usplash-artwork.so /usr/lib/usplash/usplash-theme-boxee.so 55
+    
     echo "+-------------------------------------------------+"
     echo "| All customisations applied, have a nice day! :) |"
     echo "+-------------------------------------------------+"
